33eb9010872def6392558f6c3bdde5a93075248b
vkamigo <kumawatiitkgp@gmail.com>
2020-05-05 10:51:03 +0530

    Update FinalTry2.java

--- FinalTry2.java
+++ FinalTry2.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Time;
+import java.sql.Timestamp;
 
 public class FinalTry2 {
 
@@ -21,11 +23,13 @@ public class FinalTry2 {
 			long number_L = Long.parseLong(array[2]);
 			long number_R = Long.parseLong(array[3]);
 
+			System.out.println(java.time.LocalTime.now()); 
 			String temp_number_L = Long.toBinaryString(number_L); // L ki length R kar chuke hai
 			String temp_number_R = Long.toBinaryString(number_R); // Length R rahegi
 			String temp_number_X = Long.toBinaryString(number_X);
 			String temp_number_Y = Long.toBinaryString(number_Y);
-
+			System.out.println(java.time.LocalTime.now()); 
+			
 			StringBuilder x = new StringBuilder(); // X ko R k equal banaya .
 			StringBuilder y = new StringBuilder(); // Y ko R k equal banaya .
 			StringBuilder L = new StringBuilder(); // L ko R length equal banaya .
@@ -49,19 +53,24 @@ public class FinalTry2 {
 			String X = new String(x.substring(x.length() - R.length()));
 			String Y = new String(y.substring(y.length() - R.length()));
 
-			System.out.println(L);
-
-			System.out.println(X);
-			System.out.println(Y);
-			System.out.println(R);
-
+			System.out.println("L: " +L);
+			System.out.println("O: ||||||||||||||||||||||||||||||||");
+			System.out.println("X: " +X);
+			System.out.println("O: ||||||||||||||||||||||||||||||||");
 			boolean option_flag = false;
-			int option_index = -1;
+			boolean smart_byte = false;
 
 			StringBuilder Z = new StringBuilder();
 
+			
 			for (int index = 0; index < R.length(); index++) {
-
+				
+				if(smart_byte == true) {
+					Z.append(Character.getNumericValue(X.charAt(index))
+							|Character.getNumericValue(Y.charAt(index)));
+					continue;
+				}
+				
 				if (L.charAt(index) == '0' && R.charAt(index) == '0') {
 					if (option_flag == false) {
 						Z.append(0);
@@ -69,10 +78,16 @@ public class FinalTry2 {
 						// option_index = ;
 					} else {
 						if (X.charAt(index) == '1' && Y.charAt(index) == '1') {
+							if(option_flag == true)
+								smart_byte = true;
 							Z.append(1);
 							option_flag = false;
-							Z.replace(index - 1, index - 1, "0");
+							Z.replace(index - 1, index, "0");
 							// option_index = ;
+						}
+						else {
+							Z.append(1);
+							option_flag = false;
 						}
 					}
 
@@ -85,7 +100,7 @@ public class FinalTry2 {
 						if (X.charAt(index) == '1' && Y.charAt(index) == '1') {
 							Z.append(1);
 							option_flag = false;
-							Z.replace(index - 1, index - 1, "0");
+							Z.replace(index - 1, index, "0");
 							// option_index = ;
 						}
 					}
@@ -94,7 +109,7 @@ public class FinalTry2 {
 					if (option_flag == false) {
 						if (X.charAt(index) == '0' && Y.charAt(index) == '0') {
 							Z.append(0);
-							option_flag = true;
+							smart_byte = true;
 							// option_index = ;
 						}
 						else if (X.charAt(index) == '0' && Y.charAt(index) == '1') {
@@ -143,7 +158,7 @@ public class FinalTry2 {
 				} else if (L.charAt(index) == '1' && R.charAt(index) == '1') {
 					if (option_flag == false) {
 						if (X.charAt(index) == '0' && Y.charAt(index) == '0') {
-							Z.append(0);
+							Z.append(1);
 							option_flag = true;
 							// option_index = ;
 						}
@@ -165,7 +180,7 @@ public class FinalTry2 {
 
 					} else {
 						if (X.charAt(index) == '0' && Y.charAt(index) == '0') {
-							Z.append(0);
+							Z.append(1);
 							option_flag = true;
 							// option_index = ;
 						}
@@ -184,18 +199,25 @@ public class FinalTry2 {
 							option_flag = false;
 							// option_index = ;
 						}
-
 					}
-
 				}
 			}
 
 //			Z.substring(0, R.length());
+
+			System.out.println("Z: "+ Z.substring(0, R.length()));
+			System.out.println("O: ||||||||||||||||||||||||||||||||");
+			System.out.println("Y: " +Y);
+			System.out.println("O: ||||||||||||||||||||||||||||||||");
+			System.out.println("R: " +R);
+			
 			
 			long final_result = Long.parseLong(Z.substring(0, R.length()), 2);
 
 			System.out.println(final_result);
 			//System.out.println(Z);
+			System.out.println(java.time.LocalTime.now());
 		}
 	}
 }
+\4
